33bacd027177d2eb60a3b23fa868f66940f94639
Rishabh Tiwari <rishtiwari98@gmail.com>
2025-06-05 21:53:42 +0200

    adding practice questions

---
+++ Practice Questions/Day2.py
@@ -0,0 +1,9 @@
+"""
+Reverse Full Name
+
+Write a Python program that accepts the user's first and last name and prints them in reverse order with a space between them.
+"""
+
+First_name = input("Enter your first name :")
+Last_name = input("Enter your last name :")
+print(Last_name + " " + First_name)
