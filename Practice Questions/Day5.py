42a8fa6959b67e6f4c3c8d8da0b3f7f771a857a8
Rishabh Tiwari <rishtiwari98@gmail.com>
2025-06-06 15:38:28 +0200

    Adding practice questions

---
+++ Practice Questions/Day5.py
@@ -0,0 +1,11 @@
+"""
+Write a function that returns the sum of two numbers
+"""
+
+def my_func(a, b):
+    return a + b
+
+a = float(input("Please enter the first number a : "))
+b = float(input("Please enter the second number b : "))
+result = my_func(a, b)
+print(f"The sum of two numbers is {result}")
